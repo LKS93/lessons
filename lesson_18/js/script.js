7d33dc047a77e070b371112244af67ec089ed306
LKS93 <97385113+LKS93@users.noreply.github.com>
2022-11-14 21:44:07 +0300

    Delete lesson_18 directory

--- lesson_18/js/script.js
+++
@@ -1,9 +0,0 @@
-const iconMenu = document.querySelector('.nav__icon');
-if (iconMenu) {
-    const menuBody = document.querySelector('.nav__body');
-    iconMenu.addEventListener("click", function (e) {
-        document.body.classList.toggle('--locked');
-        iconMenu.classList.toggle('nav__icon--active');
-        menuBody.classList.toggle('nav__body--active');
-    });
-}
